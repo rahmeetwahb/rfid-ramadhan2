7fe48d2b65fa6e4b048e311c7336bbc91bc6144d
Rahmat Wahab Ramadhan <rahmeet.abdulwahab@gmail.com>
2026-03-09 23:09:34 +0700

    edit suara

--- server.js
+++ server.js
@@ -20,10 +20,7 @@ dayjs.tz.setDefault("Asia/Jakarta")
 dotenv.config()
 const app = express()
 app.use(bodyParser.json())
-<<<<<<< HEAD
-=======
 app.use(express.static("public"))
->>>>>>> f2a6a2a30efe37ddf70dd5c2511fce4c6b1e7a34
 
 // ==============================
 // CEK TANGGAL
@@ -190,11 +187,8 @@ app.get("/tv", (req, res) => {
         </head>
 
         <body>
-<<<<<<< HEAD
-=======
         <audio id="successSound" src="/success.mp3"></audio>
         <audio id="deniedSound" src="/denied.mp3"></audio>
->>>>>>> f2a6a2a30efe37ddf70dd5c2511fce4c6b1e7a34
 
         <div class="name" id="name">Menunggu Scan...</div>
         <div class="session" id="session"></div>
@@ -211,11 +205,8 @@ app.get("/tv", (req, res) => {
 
             const nameEl = document.getElementById("name")
             const sessionEl = document.getElementById("session")
-<<<<<<< HEAD
-=======
             const successSound = document.getElementById("successSound")
             const deniedSound = document.getElementById("deniedSound")
->>>>>>> f2a6a2a30efe37ddf70dd5c2511fce4c6b1e7a34
 
             // hanya update jika scan baru
             if(data.time !== lastEventTime){
@@ -226,12 +217,9 @@ app.get("/tv", (req, res) => {
 
             document.body.style.background="green"
 
-<<<<<<< HEAD
-=======
             successSound.currentTime = 0
             successSound.play()
 
->>>>>>> f2a6a2a30efe37ddf70dd5c2511fce4c6b1e7a34
             nameEl.innerText = data.name
             sessionEl.innerText = "Session : " + data.session
 
@@ -241,12 +229,9 @@ app.get("/tv", (req, res) => {
 
             document.body.style.background="red"
 
-<<<<<<< HEAD
-=======
             successSound.currentTime = 0
-            successSound.play()
+            deniedSound.play()
 
->>>>>>> f2a6a2a30efe37ddf70dd5c2511fce4c6b1e7a34
             nameEl.innerText = data.message
             sessionEl.innerText = ""
 
@@ -280,8 +265,4 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log("Server jalan di port", PORT)
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> f2a6a2a30efe37ddf70dd5c2511fce4c6b1e7a34
